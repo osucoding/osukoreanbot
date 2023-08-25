bb841490e42f1fdde9df5ef1a627e9e4e21f239d
John Choi <johnchoi1003@icloud.com>
2023-08-25 00:18:38 -0400

    More detailed test case descriptions

--- test_next_command.py
+++ test_next_command.py
@@ -9,18 +9,21 @@ class TestNextCommand(unittest.TestCase):
         return super().setUp()
 
     def test_search_next_event_with_existing_date_left(self):
+        """Test with a date that exists at the front"""
         target_date = datetime.datetime(2023, 9, 4)
         event = search_next_event(target_date, self.osu_filename)
         self.assertTrue(event is not None)
         self.assertEqual(event.get_description(), 'Autumn Break Begins 10/12~13 - offices open')
 
     def test_search_next_event_with_existing_date_center(self):
+        """Test with a date that exists in the middle"""
         target_date = datetime.datetime(2023, 11, 24)
         event = search_next_event(target_date, self.osu_filename)
         self.assertTrue(event is not None)
         self.assertEqual(event.get_description(), 'Last Day of Classes')
 
     def test_search_next_event_with_existing_date_right(self):
+        """Test with a date that exists at the very end"""
         target_date = datetime.datetime(2024, 4, 22)
         event = search_next_event(target_date, self.osu_filename)
         self.assertTrue(event is not None)
